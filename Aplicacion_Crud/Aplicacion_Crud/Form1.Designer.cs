c582cc61bb629a1600260358629165e5af49732c
Wilmers <31548156+Wilmers@users.noreply.github.com>
2018-03-13 04:50:16 +0100

    Add files via upload

---
+++ Aplicacion_Crud/Aplicacion_Crud/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Aplicacion_Crud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dgLista = new System.Windows.Forms.DataGridView();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btnListadoNombre = new System.Windows.Forms.Button();
+            this.btnactulizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(516, 51);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 1;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(228, 56);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(230, 20);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(228, 130);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(230, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // dgLista
+            // 
+            this.dgLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLista.Location = new System.Drawing.Point(76, 220);
+            this.dgLista.Name = "dgLista";
+            this.dgLista.Size = new System.Drawing.Size(565, 150);
+            this.dgLista.TabIndex = 3;
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(516, 127);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 1;
+            this.btneliminar.Text = "eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btnListadoNombre
+            // 
+            this.btnListadoNombre.Location = new System.Drawing.Point(637, 132);
+            this.btnListadoNombre.Name = "btnListadoNombre";
+            this.btnListadoNombre.Size = new System.Drawing.Size(75, 23);
+            this.btnListadoNombre.TabIndex = 1;
+            this.btnListadoNombre.Text = "ListadoPorNombre";
+            this.btnListadoNombre.UseVisualStyleBackColor = true;
+            this.btnListadoNombre.Click += new System.EventHandler(this.btnListadoNombre_Click);
+            // 
+            // btnactulizar
+            // 
+            this.btnactulizar.Location = new System.Drawing.Point(637, 46);
+            this.btnactulizar.Name = "btnactulizar";
+            this.btnactulizar.Size = new System.Drawing.Size(75, 23);
+            this.btnactulizar.TabIndex = 1;
+            this.btnactulizar.Text = "actualizar";
+            this.btnactulizar.UseVisualStyleBackColor = true;
+            this.btnactulizar.Click += new System.EventHandler(this.btnactulizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 396);
+            this.Controls.Add(this.dgLista);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnactulizar);
+            this.Controls.Add(this.btnListadoNombre);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgLista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.DataGridView dgLista;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnListadoNombre;
+        private System.Windows.Forms.Button btnactulizar;
+    }
+}
+
---
+++ Aplicacion_Crud/Aplicacion_Crud/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Configuration;
+namespace Aplicacion_Crud
+{
+    public partial class Form1 : Form
+    {
+        ConnectionStringSettings cadena = ConfigurationManager.ConnectionStrings["cnSQL"];
+        SqlConnection cn = new SqlConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            cn = new SqlConnection(cadena.ConnectionString);
+        }
+
+        DataTable lisatdo()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("ups_listado_pais",cn);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("ups_agregar_pais", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@codigo", SqlDbType.Char, 3).Value = txtCodigo.Text;
+            cmd.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = txtNombre.Text;
+            cn.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show(i.ToString() + "se registro corectamente");
+            }catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                dgLista.DataSource = lisatdo();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgLista.DataSource = lisatdo();
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("ups_eliminar_pais", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@codigo", SqlDbType.Char, 3).Value = txtCodigo.Text;
+            cn.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show(i.ToString() + "registro eliminado");
+            }catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                dgLista.DataSource = lisatdo();
+            }
+
+        }
+
+        private void btnactulizar_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("usp_actualizar_pais", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@codigo", SqlDbType.Char, 3).Value = txtCodigo.Text;
+            cmd.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = txtNombre.Text;
+
+            cn.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show(i.ToString() + "registro actualizado");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                dgLista.DataSource = lisatdo();
+            }
+        }
+
+        private void btnListadoNombre_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("ups_listado_nombrepais", cn);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = txtNombre.Text;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgLista.DataSource = dt;
+
+        }
+    }
+}
